33fc171a93d598453b3c7496c2a53cd6dcb14545
DJacobson17 <davidbjacobson@gmail.com>
2022-09-30 12:16:58 -0500

    compartmentalized

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 def merge_sort(arr)
-  if arr.length < 2
-    return
-  elsif
-    arr.length % 2 != 0 
-    a = merge_sort(arr.slice(0, (((arr.length - 1) / 2) - 0.5)))
-    b = merge_sort(arr.slice(((arr.length - 1) / 2) + 0.5, arr.length - 1)
-    a > b ? arr = [a, b] : [b, a]
-  else
-    a = merge_sort(arr.slice(0, (((arr.length - 1) / 2))))
-    b = merge_sort(arr.slice(((arr.length - 1) / 2), arr.length - 1)
-    a > b ? arr = [a, b] : [b, a]
-      
-  end
+  return if arr.length < 2
+
+  a = merge_sort(arr.slice(0, (((arr.length - 1) / 2))))
+  b = merge_sort(arr.slice(((arr.length - 1) / 2), arr.length - 1))
+  merge(a, b)
+end
+
+def merge(left, right)
+  
+
+
+n = [4, 8, 3, 2]
+merge_sort(n)
