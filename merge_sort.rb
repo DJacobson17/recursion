ec821f17f55418a12c35a976b8ef192cc2f4fc46
DJacobson17 <davidbjacobson@gmail.com>
2022-10-03 09:28:46 -0500

    changed logic and simplified adding 0 index instead of iterating over copy

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,16 +1,26 @@
 # frozen_string_literal: true
+require 'pry-byebug'
 
 def merge_sort(arr)
-  return if arr.length < 2
+  return arr if arr.length < 2
 
-  a = merge_sort(arr.slice(0, (((arr.length - 1) / 2))))
-  b = merge_sort(arr.slice(((arr.length - 1) / 2), arr.length - 1))
-  merge(a, b)
+  q = arr.length / 2
+  left = merge_sort(arr[0...q])
+  right = merge_sort(arr[q...])
+  merge(left, right)
 end
 
-def merge(left, right)
-  
+def merge(left, right, array = [])
+  until left.empty? || right.empty?
+    # binding.pry
+    array << if left[0] < right[0]
+               left.shift
+             else
+               right.shift
+             end
+  end
+  array + left + right
+end
 
-
-n = [4, 8, 3, 2]
-merge_sort(n)
+a = [2, 9, 4, 7, 1]
+puts merge_sort(a)
